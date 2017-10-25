e11101b9e42eb717424c7500ad46c53b81ec0324
woxingxiao <Chauvet910@gmail.com>
2017-10-25 17:05:18 +0800

    2.0 demo updated

--- app/src/main/java/com/xw/sample/vectorcompattextview/MainActivity.java
+++ app/src/main/java/com/xw/sample/vectorcompattextview/MainActivity.java
@@ -1,7 +1,10 @@
 package com.xw.sample.vectorcompattextview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.xw.repo.VectorCompatTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final VectorCompatTextView textView = (VectorCompatTextView) findViewById(R.id.checkable_text_view);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.toggle();
+                textView.setText(textView.isChecked() ? "SELECTED TAB" : "UNSELECTED TAB");
+            }
+        });
     }
 }
